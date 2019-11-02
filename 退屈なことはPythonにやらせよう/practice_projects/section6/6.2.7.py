da35cae51688f5c6aef2d5db8c9b100daf1f2bf0
AsukiYanagihara <43675438+AsukiYanagihara@users.noreply.github.com>
2019-11-02 11:59:59 +0900

    Add files via upload

---
+++ 退屈なことはPythonにやらせよう/practice_projects/section6/6.2.7.py
@@ -0,0 +1,9 @@
+Python 3.6.5 (v3.6.5:f59c0932b4, Mar 28 2018, 17:00:18) [MSC v.1900 64 bit (AMD64)] on win32
+Type "copyright", "credits" or "license()" for more information.
+>>> import pyperclip
+>>> pyperclip.copy('Hello world')
+>>> pyperclip.paste()
+'Hello world'
+>>> pyperclip.paste()
+'ラプラス方程式コードの中身を理解する。\r\n\r\n・拡散方程式をエクセルをつかった数値計算で解いて拡散方程式を数値計算でとく方法を理解する。\r\n\r\n（・UI作成。趣'
+>>> 
